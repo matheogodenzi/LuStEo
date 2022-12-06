941cd34621ddf5fc5e629cfc0ba9edf8be2c412f
matheogodenzi <matheo.godenzi@epfl.ch>
2022-12-06 10:02:39 +0100

    modification of CNN initilization

--- 330513_311785_288687_project/main.py
+++ 330513_311785_288687_project/main.py
@@ -4,9 +4,9 @@ import utils
 import time
 
 # these will be imported in MS2. uncomment then!
-#import torch
-#from torch.utils.data import DataLoader
-#from methods.deep_network import SimpleNetwork, Trainer
+import torch
+from torch.utils.data import DataLoader
+from methods.deep_network import SimpleNetwork, Trainer
 
 from data import H36M_Dataset, FMA_Dataset, Movie_Dataset
 from methods.pca import PCA
@@ -23,19 +23,19 @@ def main(args):
         train_dataset = H36M_Dataset(split="train", path_to_data=args.path_to_data)
         test_dataset = H36M_Dataset(split="test", path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
         #uncomment for MS2
-        #val_dataset = H36M_Dataset(split="val",path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
+        val_dataset = H36M_Dataset(split="val",path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
 
     elif args.dataset=="music":
         train_dataset = FMA_Dataset(split="train", path_to_data=args.path_to_data)
         test_dataset = FMA_Dataset(split="test", path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
         #uncomment for MS2
-        #val_dataset = FMA_Dataset(split="val",path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
+        val_dataset = FMA_Dataset(split="val",path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
 
     elif args.dataset=="movies":
         train_dataset = Movie_Dataset(split="train", path_to_data=args.path_to_data)
         test_dataset = Movie_Dataset(split="test", path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
         #uncomment for MS2
-        #val_dataset = Movie_Dataset(split="val", path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
+        val_dataset = Movie_Dataset(split="val", path_to_data=args.path_to_data, means=train_dataset.means, stds=train_dataset.stds)
 
     # Note: We only use the following methods for more old-school methods, not the nn!
     train_data, train_regression_target, train_labels = train_dataset.data, train_dataset.regression_target, train_dataset.labels
@@ -62,14 +62,13 @@ def main(args):
         test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
         # create model
-        model = SimpleNetwork(input_size=train_dataset.feature_dim, num_classes=train_dataset.num_classes, regression_output_size=train_dataset.regression_target_size)
+        model = SimpleNetwork(input_size=train_dataset.feature_dim, num_classes=train_dataset.num_classes)
 
         # training loop
         trainer = Trainer(model, lr=args.lr, epochs=args.max_iters)
-        trainer.train_all(train_dataloader, val_dataloader)
-        results_class, results_reg = trainer.eval(test_dataloader)
+        ttrainer.train_all(train_dataloader, val_dataloader)
+        results_class = trainer.eval(test_dataloader)
         torch.save(results_class, "results_class.txt")
-        torch.save(results_reg, "results_reg.txt")
 
     # classical ML methods (MS1 and MS2)
     # we first create the classification/regression objects
