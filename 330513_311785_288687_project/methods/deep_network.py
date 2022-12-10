abaf4bf432a1f6169ecf0088efd75bb3a10600ba
matheogodenzi <matheo.godenzi@epfl.ch>
2022-12-10 02:11:42 +0100

    final touch

--- 330513_311785_288687_project/methods/deep_network.py
+++ 330513_311785_288687_project/methods/deep_network.py
@@ -5,7 +5,7 @@ from metrics import accuracy_fn, macrof1_fn
 import numpy as np
 
 ## MS2!!
-
+# optimal parameters found : lr = 1e-4, max-iters=1500
 
 class SimpleNetwork(nn.Module):
     """
@@ -142,7 +142,7 @@ class Trainer(object):
                 #print(it)
             self.acc = acc_run/len(dataloader.dataset)
             self.f1 = macrof1_fn(results_class.numpy(), y_tot_class.numpy())
-            print('acc =', self.acc)
-            print('f1 score =', self.f1)
+            #print('acc =', self.acc)
+            #print('f1 score =', self.f1)
 
         return results_class
